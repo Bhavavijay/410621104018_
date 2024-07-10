3a46d1cceb74976c9ef60a40ad4a495704d31ed1
Bhavavijay <112712852+Bhavavijay@users.noreply.github.com>
2024-07-10 11:38:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Avg.css';
+import logo from './logo.svg';
+import './App.css';
+import Avg from './Avg';
+import Prime from'./Prime';
+
+function App() {
+  return (
+    <div className="App">
+      <Prime />
+      <Avg />
+    </div>
+  );
+}
+
+export default App;
